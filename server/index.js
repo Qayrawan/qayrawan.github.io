4983cdda0a38ec2b30aaa979daafc8f07ebea681
Mink <gemeristhecoolest@gmail.com>
2026-04-03 15:49:57 +0300

    Serve index.html instead of plain text response

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.send("Qayrawan Beta Running");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(3000, () => {
