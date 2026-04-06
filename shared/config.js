32136f507100be869b6bc7054eaf51b54c084507
Mink <gemeristhecoolest@gmail.com>
2026-04-06 23:06:00 +0300

    Delete shared directory

--- shared/config.js
+++
@@ -1,6 +0,0 @@
-window.QAYRAWAN_CONFIG = {
-  appName: "Qayrawan",
-  schoolName: "Al-Qayrawan High School",
-  city: "Baghdad",
-  defaultLanguage: "ar"
-};
--- shared/schema.js
+++
@@ -1,6 +0,0 @@
-window.QAYRAWAN_SCHEMA = {
-  roles: ["student", "teacher", "admin"],
-  grades: ["7", "8", "9", "10", "11", "12"],
-  gradeLabelsAr: { "7": "الأول المتوسط", "8": "الثاني المتوسط", "9": "الثالث المتوسط", "10": "الرابع الإعدادي", "11": "الخامس الإعدادي", "12": "السادس الإعدادي" },
-  gradeLabelsEn: { "7": "1st Middle", "8": "2nd Middle", "9": "3rd Middle", "10": "4th Preparatory", "11": "5th Preparatory", "12": "6th Preparatory" }
-};
